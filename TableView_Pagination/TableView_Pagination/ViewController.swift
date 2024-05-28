715da368c224b77cf46f73a982cc2060dabf8d4a
Akash Jambhulkar <akash9028675325@gmail.com>
2024-05-28 20:57:20 +0530

    Created a blank project which has minimum iOS version support is 13.0

---
+++ TableView_Pagination/TableView_Pagination/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableView_Pagination
+//
+//  Created by Akash Jambhulkar on 28/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TableView_Pagination/TableView_PaginationUITests/TableView_PaginationUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TableView_PaginationUITestsLaunchTests.swift
+//  TableView_PaginationUITests
+//
+//  Created by Akash Jambhulkar on 28/05/24.
+//
+
+import XCTest
+
+final class TableView_PaginationUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
